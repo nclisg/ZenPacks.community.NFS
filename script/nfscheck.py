c82c666ca275669f7be9b9054a5c0669e5c8bf59
nickmuir <nick@nickmuir.net>
2016-04-20 09:35:59 +0100

    'add monitoring template'

---
+++ script/nfscheck.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+
+nfsd = '/proc/net/rpc/nfsd' 
+
+data = open(nfsd)
+
+elements = []
+
+for line in data:
+    if line.startswith('proc3'):
+        elements = line.split()
+
+print "OK | getattr=%s, setattr=%s, lookup=%s, access=%s, readlink=%s, read=%s, write=%s, create=%s, mkdir=%s, symlink=%s, mknod=%s, remove=%s,rmdir=%s, rename=%s, link=%s, readdir=%s, readdirplus=%s, fsstat=%s, fsinfo=%s, pathconf=%s, commit=%s" % (elements[2], elements[3], elements[4], elements[5], elements[6], elements[7], elements[8], elements[9], elements[10], elements[11], elements[12], elements[13], elements[14], elements[15], elements[16], elements[17], elements[18], elements[19], elements[20], elements[21], elements[22])
+
--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.NFS"
-VERSION = "1.0.0dev"
-AUTHOR = "Your Name Here"
-LICENSE = ""
+VERSION = "1.0.0-beta"
+AUTHOR = "Nick Muir"
+LICENSE = "GPLv3"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
 PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.NFS']
 INSTALL_REQUIRES = []
